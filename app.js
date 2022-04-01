929aad666a1ce725e0ff8c7eafcc9db8cad8a126
Joaquin Cisneros <2014joaquincisneros@gmail.com>
2022-04-01 12:35:42 -0700

    Finished signup and css, set up post method and deleted request package

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 
 
 const express = require("express");
-const request = require("request");
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/", function(req,res){
+  var fName = req.body.firstName;
+  var lName = req.body.lastName;
+  var email = req.body.email;
+
+  console.log(fName,lName,email);
+})
 
 app.listen(3000, function(){
   console.log("test");
