da8482f1fa7bf3d4db2ffcffff55ed61bfe71bb1
Joaquin Cisneros <2014joaquincisneros@gmail.com>
2022-04-01 11:45:33 -0700

    Gammer fixed readme, added bootstrap template for css and signup. app in

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-dsa
+
+
+const express = require("express");
+const request = require("request");
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.listen(3000, function(){
+  console.log("test");
+})
