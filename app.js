17d8a2b5fef4b7ad68e0d895164cadb22b3c59a0
Joaquin Cisneros <2014joaquincisneros@gmail.com>
2022-04-01 17:41:33 -0700

    Added code to get mail chimp working. What a pain in the ***

--- app.js
+++ app.js
@@ -9,21 +9,42 @@ app.use(express.static(__dirname));
 
 app.get("/", function(req,res){
   res.sendFile(__dirname+"/signup.html");
-})
+});
+
+mailchimp.setConfig({
+  apiKey: "eb3f8a5f0b617c41dde54b958aa7734d-us14",
+  server: "us14"
+});
 
 app.post("/", function(req,res){
   var fName = req.body.firstName;
   var lName = req.body.lastName;
   var email = req.body.email;
 
+  const listId = "f7165473eb";
+  const user = {
+    firstName:fName,
+    lastName:lName,
+    email:email
+  };
 
+  async function run(){
+    const response = await mailchimp.lists.addListMember(listId,{
+      email_address: user.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: user.firstName,
+        LNAME: user.lastName
+      }
+    });
 
-})
+    res.sendFile(__dirname+"/success.html");
+    console.log("User has been added to the mailing list");
+
+  }run().catch(e => res.sendFile(__dirname+"/failure.html"));
+
+});
 
 app.listen(3000, function(){
   console.log("test");
-})
-
-// API Key
-// eb3f8a5f0b617c41dde54b958aa7734d-us14
-// list id: f7165473eb
+});
